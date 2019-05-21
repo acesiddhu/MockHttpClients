923102bf21193163f0211534207da3b48690ed54
siddhap <siddhap@microsoft.com>
2019-05-21 12:18:04 +0530

    first commit

---
+++ UnitTestProject25/UnitTestProject25/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject25")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject25")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c5fd15f-32cd-484d-bb84-7a3d34cb6e19")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject25/UnitTestProject25/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.TestResults.WebApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestProject25
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var m1 = Mock.Of<GitHttpClient>();
+            //var m = Mock.Get(m1);
+            var m = new Mock<GitHttpClient>(new Uri("https://mseng.visualstudio.com"), null);
+            //var m1 = new Mock<BuildHttpClient>(new Uri("https://mseng.visualstudio.com"), null);
+            var o = m.Object;
+            //var o1 = m1.Object;
+
+            //m.SetupAllProperties();
+
+        }
+    }
+}
